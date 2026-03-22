8fcf8b7c48e71900dcfff6411c3f2d4987647a78
xemx86 <117552537+xemx86@users.noreply.github.com>
2026-03-22 16:04:17 -0400

    Update middleware.ts

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -1,7 +1,7 @@
+import { NextRequest, NextResponse } from "next/server";
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
-import { NextResponse, type NextRequest } from "next/server";
 
-export async function updateSession(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   let response = NextResponse.next({
     request,
   });
@@ -41,3 +41,9 @@ export async function updateSession(request: NextRequest) {
 
   return response;
 }
+
+export const config = {
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
